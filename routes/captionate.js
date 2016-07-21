7cd3cdb62bb34d3827226399f7ab77e0d9ea1b5e
Fernando Toffolo <fmtoffolo@gmail.com>
2016-07-21 00:58:51 -0300

    first

---
+++ routes/captionate.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var captionate = require('../../adCaption');
+
+function parseQuery(query) {
+    var configuration = [];
+
+    for (var key in query) {
+        var temp = {};
+
+        var values = query[key].split('|');
+
+        values = values.map(function(values) {
+                if (values.substring(0, 'imageUrl'.length) === 'imageUrl') {
+                    return ['imageUrl', values.substring('imageUrl:'.length, values.length)];
+                }
+                if (values.substring(0, 'text'.length) === 'text') {
+                    return ['text', values.substring('text:'.length, values.length)];
+                }
+                var config = values.split(':');
+                return config;
+            })
+            .reduce(function(con, data) {
+                con[data[0]] = data[1];
+                return con;
+            }, temp);
+
+        configuration.push(temp);
+    }
+
+    return configuration;
+}
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+
+    captionate(parseQuery(req.query))
+        .then(function(buffer) {
+            res.contentType('png');
+            res.end(buffer);
+        })
+        .catch(function(error) {
+            console.log(error);
+            res.send(error.toString());
+        });
+});
+
+
+module.exports = router;
